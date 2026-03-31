a62a11aabe9dc074ce191ad5ff0002019567d88e
Ludmila Tawane <160486314+16lud@users.noreply.github.com>
2026-03-30 21:47:47 -0300

    Update q4.ts

--- q4.ts
+++ q4.ts
@@ -1,1 +1,8 @@
-let data
+const aluno = {
+    nome: "Ludmila",
+    matricula: "202012345678",
+    curso: "Informática",
+    ira: 83.2
+};
+
+console.log(`${aluno.nome} é aluna de ${aluno.curso}, matrícula ${aluno.matricula}, IRA ${aluno.ira}`);
