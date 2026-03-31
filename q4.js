835536be0763ab4b99cfc4a7bbb63b4d2a8bb71d
Ludmila Tawane <160486314+16lud@users.noreply.github.com>
2026-03-30 21:46:39 -0300

    Add Q4 class with main method and student info

---
+++ q4.js
@@ -0,0 +1,12 @@
+public class Q4 {
+    public static void main(String[] args) {
+        String nome = "Ludmila";
+        String matricula = "202012345678";
+        String curso = "Informática";
+        double ira = 83.2;
+
+        System.out.println(nome + " é aluna de " + curso +
+                ", matrícula " + matricula +
+                ", IRA " + ira);
+    }
+}
